a2beb7ef933727205597561186b20533edd1654f
MirandaArroyo <mirandarroyo@gmail.com>
2018-02-25 18:32:22 +0100

    practica ajax

--- js/buscador.js
+++ js/buscador.js
@@ -7,7 +7,7 @@
                if((val.nombre.search(regEx) !=-1)){
                    output+="<tr>"
                    output+="<td id='"+key+"'>"+val.nombre+"</td>"
-                   output+="<td id='"+key+"'>"+val.precio+"</td>"
+                   //output+="<td id='"+key+"'>"+val.precio+"</td>"
                    output+="</tr>"
                    if(document.getElementById("search").value===""){
                         $("table").hide();
--- js/scriptormulario.js
+++ js/scriptormulario.js
@@ -41,6 +41,8 @@ function validaformulario(){
     }
     //validar tarjeta
     tarjeta=parseInt(tarjeta);
+    
+   // muestraCampo();
     if(tarjeta && !visa.test(tarjeta) && !mastercard.test(tarjeta)){     
             todoCorrecto=false;
             mensaje+="El numero de tarjeta no es correcto";
@@ -48,8 +50,6 @@ function validaformulario(){
     
     if(todoCorrecto){
         alert("se han registrado los datos correctamente");
-        //document.cookie = name=nombre;
-        //document.cookie = password=contrasenia1;
         guardarCookie("name", nombre, 2);
         guardarCookie("password", haseada, 2);
     }else{
@@ -57,49 +57,86 @@ function validaformulario(){
     }
     
 }
+function buscaProductos() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
 //con esta funcion mostraremos el campo de la tarjeta de credito si el usuario ha introdicido direccion
-function muestraCampo(){
-    var direccion= document.getElementById("direccion").value;
+/*var contenedor= document.getElementById("Campotarjeta");
+    var inputTarjeta=document.createElement('INPUT');
+    inputTarjeta.setAttribute("id", "tarjeta");
+inputTarjeta.setAttribute("type", "text");
+    contenedor.appendChild(inputTarjeta);*/
+    
+/*function muestraCampo(){
+    
+        
+    var direccion= document.getElementById("direccion").value;/*
+    var contenedor= document.getElementById("Campotarjeta");
+    var inputTarjeta=document.createElement('INPUT');
+    inputTarjeta.type='TEXT';
+    inputTarjeta.id='tarjeta';
+    contenedor.appendChild(inputTarjeta);
     
     if(direccion!=""){
-        document.getElementById("Campotarjeta").style.display='block';
+        
+     document.getElementById("Campotarjeta").style.display='block';
     }else{
+        //contenedor.removeChild(contenedor.childNodes[0]);
         document.getElementById("Campotarjeta").style.display='none';
     }
-}
+    
+
+}*/
 
 function campoPaises(){
-    var state = new Array("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antarctica", "Antigua and Barbuda",
-    "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
-    "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana",
-    "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-    "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic",
-    "Congo, Republic of the", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark",
-    "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
-    "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana",
-    "Greece", "Greenland", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong",
-    "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan",
-    "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
-    "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar",
-    "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
-    "Moldova", "Mongolia", "Morocco", "Monaco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand",
-    "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-    "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "San Marino", " Sao Tome",
-    "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia",
-    "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden",
-    "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago",
-    "Tunisia", "Turkey", "Turkmenistan", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
-    "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe");
-    var paises= document.getElementById("pais"),option;
     
-    for(var hi=0; hi<state.length; hi++){
-         option= document.createElement("option");
-        option.setAttribute("value", state[hi]);
-        option.innerHTML=state[hi];
-        paises.appendChild(option);
-    }
-}
+            var cajaPaises= document.getElementById("pais"),option;
+            var paisesUrl ="https://raw.githubusercontent.com/umpirsky/country-list/master/data/es/country.json";
 
+        var getJSON = function(url) {
+          return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', url, true);
+            xhr.responseType = 'json';
+            xhr.onload = function() {
+              var status = xhr.status;
+              var readyState = xhr.readyState;   
+              if (status == 200 && readyState==4) {
+                resolve(xhr.response);
+              } else {
+                reject(status);
+              }
+            };
+            xhr.send();
+          });
+        };
+
+        getJSON(paisesUrl).then(function(data) {
+         for(var i in data)   {
+             option= document.createElement("option");
+             option.setAttribute("value", i);
+             option.innerHTML=i;
+             cajaPaises.appendChild(option);
+            
+         }
+          
+        }, function(status) {
+          alert('Error...');
+        }); 
+}
 
 function guardarCookie(nombreCookie, valorCookie, diasExpirar) {
 	var fecha = new Date();
@@ -148,12 +185,11 @@ function cerrarSesion(){
 }
 
 
-
 if(obtenerCookie("logueado")=="true"){
     var nombreUsuario=obtenerCookie("name");
      document.getElementById('inicioSesion').innerHTML=nombreUsuario;
 }
-
+/*
 $("i.fa.fa-heart").click(function() {
   if(obtenerCookie("logueado")=="true"){
         var contador= $(this).html();
@@ -174,3 +210,93 @@ $("i.fa.fa-star").click(function() {
         $(this).css("color", "yellow");
     }
 });
+
+//Creacion de caja de comentarios con DOM
+var comentarios =document.getElementById("cajaComentarios");
+if(obtenerCookie("logueado")=="true"){
+    generaCajaComentarios();
+            
+}
+generaComentarios();
+
+function muestramelo()
+{
+	var vista=document.getElementById("cajaComentarios").style.display;
+	if(vista=='none'){
+		vista='block';
+	}else{
+		vista='none';
+    }
+
+	document.getElementById("cajaComentarios").style.display = vista;
+}
+
+function generaComentarios(){
+    var comentarios = document.getElementById("cajaComentarios");
+    var parrafo = document.createElement('p');
+    var parrafo2 = document.createElement('p');
+    var parrafo3 = document.createElement('p');
+    var texto = document.createTextNode('Paco: muy bueno!');
+    var texto2 = document.createTextNode('Pepe: riquisimo!');
+    var texto3 = document.createTextNode('Lola: genial!');
+    parrafo.appendChild(texto);
+    parrafo2.appendChild(texto2);
+    parrafo3.appendChild(texto3);
+    comentarios.appendChild(parrafo);
+    comentarios.appendChild(parrafo2);
+    comentarios.appendChild(parrafo3);
+
+}
+function generaCajaComentarios(){
+    var comentarios=document.getElementById("cajaComentarios");
+        
+        var formulario=document.createElement('FORM');
+        formulario.name='formularioComentarios';
+        formulario.id='formularioComentarios';
+        formulario.method='POST';
+        
+        var miInput=document.createElement('INPUT');
+        miInput.type='TEXT';
+        miInput.id='comentario';
+        miInput.name='comentario';
+        
+        //boton de envio:
+        var enviar = document.createElement("button");
+        var t = document.createTextNode("Enviar comentario");
+        //añadimos estilos al boton
+        enviar.setAttribute("type", "submit");
+        enviar.setAttribute("id", "enviarComentario");
+        enviar.style.backgroundColor="#ff80bf";
+        enviar.style.color="white";
+        
+        enviar.appendChild(t); 
+        comentarios.appendChild(formulario);
+        formulario.appendChild(miInput);
+        formulario.appendChild(enviar);
+        
+        document.getElementById("enviarComentario").addEventListener('click', function(e){ 
+            muestraComentario();
+            //sumamos 1 cada vez que el susuario genera un comentario
+            var contador= $("i.fa.fa-comment").html();
+            contador=parseInt(contador);
+            contador++;
+            $("i.fa.fa-comment").text(contador);
+            $("i.fa.fa-comment").css("color", "grey");
+
+            e.preventDefault();
+        });
+        
+}
+function muestraComentario(){
+    var nombre= obtenerCookie("name");
+    var com= document.getElementById("comentario").value;
+    var comentarios = document.getElementById("cajaComentarios");
+    var parrafo = document.createElement('p');
+    var texto = document.createTextNode(nombre+": "+com);
+    var usuario = document.createTextNode(nombre);
+    parrafo.appendChild(texto);
+    comentarios.insertBefore(parrafo, comentarios.childNodes[1]); 
+    
+}
+
+*/
